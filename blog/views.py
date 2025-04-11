cb529c4f021c92f2d0ca1f0429eb8f6e1d70b77a
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-04-11 05:35:54 +0300

    serialize_post_optimized

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,22 @@ def serialize_post(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
-        'author': post.author.username, 
-        'comments_amount': len(Comment.objects.filter(post=post)), 
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title if post.tags.all() else '',
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': len(Comment.objects.filter(post=post)),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -47,8 +61,8 @@ def index(request):
     )
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
@@ -56,7 +70,6 @@ def index(request):
 
 def post_detail(request, slug):
     post = Post.objects.select_related('author').prefetch_related('tags').get(slug=slug)
-    
     comments = Comment.objects.filter(post=post).select_related('author')
     serialized_comments = []
     for comment in comments:
